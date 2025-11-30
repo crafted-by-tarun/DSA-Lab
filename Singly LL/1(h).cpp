64c75ad269017fab514a2e64a97894a872eae8b1
Tarun Mahindra <gfxgenix@gmail.com>
2025-11-30 14:19:23 +0530

    Add singly linked list implementation in C++
    Implement a singly linked list with insertion and display methods.

---
+++ Singly LL/1(h).cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class SLL {
+public:
+    struct Node {
+        int data;
+        Node* next;
+        Node(int x) : data(x), next(NULL) {}
+    };
+
+    Node* head = NULL;
+
+    void insertEnd(int x) {
+        Node* n = new Node(x);
+        if(!head) head = n;
+        else {
+            Node* t = head;
+            while(t->next) t = t->next;
+            t->next = n;
+        }
+    }
+
+    void display() {
+        Node* t = head;
+        while(t) {
+            cout << t->data << " ";
+            t = t->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    SLL s;
+    s.insertEnd(10);
+    s.insertEnd(20);
+    s.insertEnd(30);
+
+    s.display();
+}
