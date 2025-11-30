9395887d58304b79e90ff166699c31f5e125b24e
Tarun Mahindra <gfxgenix@gmail.com>
2025-11-30 13:57:46 +0530

    Implement Quick Sort algorithm in Quick_sort.cpp

---
+++ Sorting/Quick_sort.cpp
@@ -0,0 +1,21 @@
+int partition(vector<int> &a, int low, int high) {
+    int pivot = a[high];
+    int i = low - 1;
+
+    for(int j = low; j < high; j++) {
+        if(a[j] < pivot) {
+            i++;
+            swap(a[i], a[j]);
+        }
+    }
+    swap(a[i+1], a[high]);
+    return i+1;
+}
+
+void quickSort(vector<int> &a, int low, int high) {
+    if(low < high) {
+        int p = partition(a, low, high);
+        quickSort(a, low, p-1);
+        quickSort(a, p+1, high);
+    }
+}
