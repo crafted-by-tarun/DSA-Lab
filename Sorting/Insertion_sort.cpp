01efdcc269190d3b95f11d82077704e2975a4ce2
Tarun Mahindra <gfxgenix@gmail.com>
2025-11-30 13:56:33 +0530

    Create Insertion_sort.cpp

---
+++ Sorting/Insertion_sort.cpp
@@ -0,0 +1,13 @@
+void insertionSort(vector<int> &a) {
+    int n = a.size();
+    for(int i = 1; i < n; i++) {
+        int key = a[i];
+        int j = i - 1;
+
+        while(j >= 0 && a[j] > key) {
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = key;
+    }
+}
