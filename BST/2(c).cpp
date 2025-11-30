00ea08a7160e9a1db77e22915d6d0d50b3c47bd1
Tarun Mahindra <gfxgenix@gmail.com>
2025-11-30 13:51:03 +0530

    Add BST implementation with min function
    Implement a binary search tree with a function to find the minimum element.

---
+++ BST/2(c).cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+int data;
+Node* left;
+Node* right;
+Node(int a){
+    data = a;
+    left = right =NULL;
+}
+};
+
+class BST{
+public:
+Node* root;
+BST(){
+    root=NULL;
+}
+
+};
+int min(Node* root){
+    if(root==NULL) return-1;
+    while(root->left!=NULL){
+        root = root->left;
+    }
+    return root->data;
+}
+int main(){
+BST tree;
+tree.root = new Node(8);
+tree.root->left = new Node(3);
+tree.root->right = new Node(10);
+tree.root->left->left = new Node(1);
+tree.root->left->right = new Node(6);
+tree.root->left->right->left = new Node(4);
+tree.root->left->right->right = new Node(7);
+tree.root->right->right = new Node(14);
+tree.root->right->right->left = new Node(13);
+if(min(tree.root)==-1) cout<<"The tree is empty."<<endl;
+else{
+    cout<<"The minimum element is: "<<min(tree.root)<<endl;
+}
+
+
+
+
+
+
+
+    return 0;
+}
